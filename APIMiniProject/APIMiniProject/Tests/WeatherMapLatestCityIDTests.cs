db350172092c07a0285d800fe36929e2f7d65713
Oliver Tomkins <otomkins@yahoo.co.uk>
2021-02-03 19:44:36 +0000

    Added City ID Tests

--- APIMiniProject/APIMiniProject/Tests/WeatherMapLatestCityIDTests.cs
+++ APIMiniProject/APIMiniProject/Tests/WeatherMapLatestCityIDTests.cs
@@ -5,14 +5,46 @@ namespace APIMiniProject
 {
     public class WeatherMapLatestCityIDTests
     {
-        public WeatherMapService service;
+        public WeatherMapService serviceTest;
+        public WeatherMapService serviceTest2;
+        public WeatherMapService serviceTest3;
 
-        [Test]
-        public void WebCallSuccessCheck()
+        [OneTimeSetUp]
+        public void SinglePostcodeServiceSetup()
         {
-            service = new WeatherMapService(833);
-            Assert.That(service.DTO.LatestWeather.name.ToString(), Is.EqualTo(""));
+            serviceTest = new WeatherMapService(2655603);
         }
 
+        [Test]
+        public void SuccesfulRequestByCityIdReturns200()
+        {
+            Assert.That(serviceTest.DTO.LatestWeather.cod, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void RequestNotFoundByCityIdReturns400()
+        {
+            serviceTest2 = new WeatherMapService(-1);
+            Assert.That(serviceTest2.DTO.LatestWeather.cod, Is.EqualTo(400));
+        }
+
+        [Test]
+        public void RequestNotFoundByCityIdReturns404()
+        {
+            serviceTest3 = new WeatherMapService(37188);
+            Assert.That(serviceTest3.DTO.LatestWeather.cod, Is.EqualTo(404));
+        }
+
+        [Test]
+        public void JSONTest_ReturnsCorrectCountryGivenID()
+        {
+            Assert.That(serviceTest.DTO.LatestWeather.sys.country.ToString(), Is.EqualTo("GB"));
+        }
+
+        [Test]
+        public void JSONTest_ReturnsCorrectCountryNameGivenID()
+        {
+            Assert.That(serviceTest.DTO.LatestWeather.name.ToString(), Is.EqualTo("Birmingham"));
+        }
     }
 }
