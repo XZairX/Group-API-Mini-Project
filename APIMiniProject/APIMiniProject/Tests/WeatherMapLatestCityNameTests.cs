ee93827b14d268026b3214e1122060c6f863b1f4
Zair <michaelxzr@gmail.com>
2021-02-03 18:19:34 +0000

    Update WeatherMapLatestCityNameTests.cs

--- APIMiniProject/APIMiniProject/Tests/WeatherMapLatestCityNameTests.cs
+++ APIMiniProject/APIMiniProject/Tests/WeatherMapLatestCityNameTests.cs
@@ -14,13 +14,30 @@ namespace APIMiniProject
             _service = new WeatherMapService(_city);
         }
 
-        [Test]
-        public void CityNameQuery_ReturnsCity()
+        private WeatherMapService CreateServiceWithArgumentCity(string city)
         {
-            var result = _service.DTO.LatestWeather.name;
+            return new WeatherMapService(city);
+        }
+
+        [Ignore("In progress")]
+        [Test]
+        public void CityNameQuery_InvalidCity_ReturnsError404()
+        {
+            string invalidCity = "invalidCity";
+
+            var result = new WeatherMapService(invalidCity);
+
+            Assert.That(result.ToString(), Is.EqualTo(""));
+        }
+
+        [Test]
+        public void CityNameQuery_CityIsValidAndHasUppercaseFirstLetter_ReturnsCity()
+        {
+            var sut = CreateServiceWithArgumentCity(_city);
+
+            var result = sut.DTO.LatestWeather.name;
 
             Assert.That(result, Is.EqualTo(_city));
         }
-
     }
 }
