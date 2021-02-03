15065521329b388820fea6e53d47d2d915dd8448
Alex Marks <71220360+alexpmarks7@users.noreply.github.com>
2021-02-03 16:38:01 +0000

    Add API call methods

---
+++ APIMiniProject/APIMiniProject/Tests/WeatherMapLatestCityNameTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using APIMiniProject.HTTPManager;
+
+namespace APIMiniProject
+{
+    public class WeatherMapLatestCityNameTests
+    {
+        public WeatherMapService service;
+
+        [Test]
+        public void WebCallSuccessCheck()
+        {
+            service = new WeatherMapService("Birmingham");
+            Assert.That(service.DTO.LatestWeather.name.ToString(), Is.EqualTo(""));
+        }
+
+    }
+}
